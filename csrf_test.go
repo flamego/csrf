307cdf294dccc963934d369e4f34e732aa193507
Joe Chen <jc@unknwon.io>
2021-11-28 12:57:19 +0800

    mod: update github.com/flamego/flamego

--- csrf_test.go
+++ csrf_test.go
@@ -264,9 +264,9 @@ func TestTokenExpired(t *testing.T) {
 
 	f.Get("/touch", func(x CSRF) string { return x.Token() })
 	f.Post("/set-expired", Validate, func(s session.Session) {
-		// NOTE: Time seems flaky within one second on Windows, so let's be a little bit
-		//  more extreme.
-		s.Set(tokenExpiredAtKey, time.Now().Add(-1*time.Second))
+		// NOTE: Time seems flaky within few seconds on Windows in CI, so let's be a
+		// little more extreme.
+		s.Set(tokenExpiredAtKey, time.Now().Add(-2*time.Second))
 	})
 
 	resp := httptest.NewRecorder()
