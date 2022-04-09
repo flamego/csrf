efd2f07f9bfcd878e3f9e157a7ef70a1936f5340
songzhibin97 <718428482@qq.com>
2022-04-09 10:45:27 +0800

    Update csrf.go

--- csrf.go
+++ csrf.go
@@ -103,19 +103,19 @@ type Options struct {
 	ErrorFunc func(w http.ResponseWriter)
 }
 
-const (
-	letterBytes   = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
 var src = rand.NewSource(time.Now().UnixNano())
 
 // randomBytes generates n random []byte.
 func randomBytes(n int) []byte {
+	const (
+		letterBytes   = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+		letterIdxBits = 6                    // 6 bits to represent a letter index
+		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	)
+
 	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+
 	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
 			cache, remain = src.Int63(), letterIdxMax
